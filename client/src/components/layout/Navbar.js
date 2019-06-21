5e2c496edbc53ac30b7107f086c8f0d3dbce168d
thomas <nealthom@gmail.com>
2019-06-21 17:27:33 -0500

    Added profiles list

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,6 +8,9 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to="/profiles">Other Users</Link>
+      </li>
+      <li>
         <Link to="/dashboard">
           <i className="fas fa-user" />{" "}
           <span className="hide-sm"> Dashboard</span>
@@ -25,7 +28,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const guestLinks = (
     <ul>
       <li>
-        <Link to="#!">Developers</Link>
+        <Link to="/profiles">Other Users</Link>
       </li>
       <li>
         <Link to="/register">Register</Link>
