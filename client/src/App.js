3f04c28f8324afe9b11faf68eb5ba30010b196dd
thomas <nealthom@gmail.com>
2019-06-26 17:32:33 -0500

    able to load profile data into profile

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import AddEducation from "./components/profile-forms/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import setAuthToken from "./utils/setAuthToken";
 import PrivateRoute from "./components/routing/PrivateRoute";
+import Profile from "./components/profile/Profile";
 
 // Redux
 import { Provider } from "react-redux";
@@ -42,6 +43,7 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:id" component={Profile} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute
                 exact
