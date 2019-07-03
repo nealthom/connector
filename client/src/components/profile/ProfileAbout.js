49f6fbd8da27a7a45f4c01e41944e53e40d17e00
thomas <nealthom@gmail.com>
2019-07-03 07:07:13 -0500

    added to Profile

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -11,7 +11,7 @@ const ProfileAbout = ({
   <div class="profile-about bg-light p-2">
     {bio && (
       <Fragment>
-        <h2 className="text-primary">{name.trim().split(" ")[0]}s Bio</h2>
+        <h2 className="text-primary">My Bio</h2>
         <p>{bio}</p>
         <div className="line" />
       </Fragment>
