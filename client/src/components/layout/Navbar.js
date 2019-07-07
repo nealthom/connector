db16bd613860deddc4ffb6986a6f2f34667e2d6f
thomas <nealthom@gmail.com>
2019-07-06 19:49:34 -0500

    Added post capability

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -11,6 +11,9 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
         <Link to="/profiles">Other Users</Link>
       </li>
       <li>
+        <Link to="/posts">Posts</Link>
+      </li>
+      <li>
         <Link to="/dashboard">
           <i className="fas fa-user" />{" "}
           <span className="hide-sm"> Dashboard</span>
--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -8,7 +8,7 @@ const ProfileAbout = ({
     user: { name }
   }
 }) => (
-  <div class="profile-about bg-light p-2">
+  <div className="profile-about bg-light p-2">
     {bio && (
       <Fragment>
         <h2 className="text-primary">My Bio</h2>
